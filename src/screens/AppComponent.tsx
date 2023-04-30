b0a4b6adec946f623ea18c6017c5a396584b6688
Nisha Kumari <t1kuni01@students.oamk.fi>
2023-04-30 17:15:18 +0300

    app component updated

--- src/screens/AppComponent.tsx
+++ src/screens/AppComponent.tsx
@@ -7,26 +7,52 @@ import { getHeaderTitle } from '@react-navigation/elements';
 import HeaderComponent from "../components/HeaderComponent";
 import PickupDeliveryComponent from "./PickupDeliveryScreen";
 import PickupDeliveryScreen from "./PickupDeliveryScreen";
+import QrScanScreen from "./QrScanScreen";
 const Stack = createNativeStackNavigator();
+import { MMKV } from 'react-native-mmkv';
+import ItemViewerScreen from "./ItemViewerScreen";
+import CartScreen from "./CartScreen";
+import OrderScreen from "./OrderScreen";
+import LoginScreen from "./LoginScreen";
+import RegisterScreen from "./RegisterScreen";
+import ResetPasswordScreen from "./ResetPasswordScreen";
+import OrderStatus from "./OrderStatus";
 
 const AppComponent = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} options={{
+            <Stack.Navigator initialRouteName="qrScan">
+                <Stack.Screen name="home" component={HomeScreen} options={{
                     header: ({ navigation, route, options, back }) => {
                         const title = getHeaderTitle(options, route.name);
                         return (
-                            <HeaderComponent/>
+                            <HeaderComponent navigation={navigation}/>
                         );
                     },
                     gestureDirection: "horizontal"
                 }}/>
                 <Stack.Screen name="pickupDelivery" component={PickupDeliveryScreen} options={{}}/>
+                <Stack.Screen name="itemViewer" component={ItemViewerScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="cartScreen" component={CartScreen} options={{
+                    title : "cart"
+                }}/>
+                <Stack.Screen name="orderScreen" component={OrderScreen} options={{
+                    title : "order"
+                }}/>
+                <Stack.Screen name="login" component={LoginScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="register" component={RegisterScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="forgot" component={ResetPasswordScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="qrScan" component={QrScanScreen} options={{headerShown: false}} />
+                <Stack.Screen name="orderStatus" component={OrderStatus} options={{
+                    title : "order status"
+                }} />
             </Stack.Navigator>
         </NavigationContainer>
 
     );
 };
 
+
+export const storage = new MMKV();
+storage.set('cart', JSON.stringify('[]'));
 export default AppComponent;
