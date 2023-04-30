9aa387f00633c8acca073f6835800ea46d08f561
AnuR01 <raw.anu@outlook.com>
2023-04-30 14:36:04 +0300

    config update:debuggin in usb and android emulator

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
 export class config{
+    // For Debugging in USB
+    // public static SERVER_BASE_URL = 'http://xxx.xxx.xx.xx:4000';
+    // For Android Emulator
     public static SERVER_BASE_URL = 'http://xxx.xxx.xx:4000';
 }
