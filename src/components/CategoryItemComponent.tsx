9f7e66254f05e9e36df37c604e1686843cbeabf8
Vanshika-Saini <74698779+Vanshika-Saini@users.noreply.github.com>
2023-04-30 16:45:23 +0530

    Item Component Updated

--- src/components/CategoryItemComponent.tsx
+++ src/components/CategoryItemComponent.tsx
@@ -1,12 +1,29 @@
 import {Button, Card, Image} from "@rneui/themed";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, ToastAndroid, View} from "react-native";
 import React from "react";
 
-const CategoryItemComponent = (props: any) => {
-    return (
-        <Button size="md" containerStyle={styles.button} title={props.title}>
-        </Button>
-    );
+const CategoryItemComponent = (props) => {
+
+    const showToast = (message: string) => {
+        ToastAndroid.show(message, ToastAndroid.SHORT);
+    };
+
+    if(props.categorySelected === props.item.name)
+        return (
+            <Button size="md" color="rgba(37, 211, 102, 1)" containerStyle={styles.buttonActive} title={props.item.name} onPress={() => {
+                props.sendDataToParent(props.item.name)
+                showToast(`Showing dishes with category ${props.item.name}`)
+            }}>
+            </Button>
+        );
+    else
+        return (
+            <Button size="md" containerStyle={styles.button} title={props.item.name} onPress={() => {
+                props.sendDataToParent(props.item.name)
+                showToast(`Showing dishes with category ${props.item.name}`)
+            }}>
+            </Button>
+        );
 };
 
 const styles = StyleSheet.create({
@@ -14,7 +31,30 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         margin: 10,
-        borderRadius: 5
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    buttonActive: {
+        backgroundColor:'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        borderRadius: 5,
+        shadowColor: 'rgba(37, 211, 102, 1)',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
     heading:{
         color: 'black',
